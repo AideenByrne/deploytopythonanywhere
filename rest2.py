dec04a4e9665e27ebf24a7adc8834dc4dc3843c3
AideenByrne <g00364780@gmit.ie>
2019-11-28 20:40:24 +0000

    test change2

--- rest2.py
+++ rest2.py
@@ -1,6 +1,6 @@
 #Aideen Byrne Big Project
 #!flask/bin/python
-from flask import Flask, jsonify,  request, abort, make_response
+from flask import Flask
 
 app = Flask(__name__, static_url_path='', static_folder='.')
 
